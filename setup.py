17b27417b5afc94692a94ec9244cfefe82d67db2
Sebastien234 <104589601+Sebastien234@users.noreply.github.com>
2022-11-01 22:45:07 +0000

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='OpenSearchRec',
-    version='0.1.0',
+    version='0.1.2',
     author='Sebastien L',
     packages=find_packages(
         include="OpenSearchRec*"
